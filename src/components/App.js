3a485d07e4b1788a4142d71ea3e22d1b4aa31d48
shivangijb <37670360+shivangijb@users.noreply.github.com>
2020-12-21 18:26:43 +0530

    Changes done

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,46 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  const [timer,setTimer] = useState(0)
+  const [start,setStart] = useState(false)
+
+  function tick(){
+    const tCount = timer-1
+    setTimer(tCount)
+  }
+
+  function handleKeyDown(event){
+    if (event.keyCode === 13){
+      const tCount = Math.floor(event.target.value)
+      
+      if(typeof(tCount) === "number" && tCount > 0){
+        setTimer(tCount)
+        setStart(true) 
+      }else{
+        setTimer(0)
+      }
+    }
+  }
+
+  useEffect(()=>{
+    if (timer===0){
+      setStart(false)
+    }
+    if (start){
+      const interval = setInterval(tick, 1000);
+      return () => clearInterval(interval);
+    }
+    
+  },[start,timer])
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={handleKeyDown} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{timer}</div>
     </div>
   )
 }
